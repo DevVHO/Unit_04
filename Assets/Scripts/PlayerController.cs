8a83d3be031beb07a471f15814f6871247e0d9dd
DevVHO <161318358+DevVHO@users.noreply.github.com>
2024-06-25 09:11:22 +0100

    Lesson 4.3 - PowerUp and CountDown
    Basicamente nessa etapa estive adicionando power-ups ao cenário para que assim eu consiga benefícios ao coleta-los

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private float powerUpStrength = 15.0f;
     public float speed = 2.0f;
+    public bool hasPowerup = false;
+    public GameObject powerUpIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +24,36 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerUpIndicator.transform.position = transform.position + new Vector3(0,-0.5f,0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUps"))
+        {
+            hasPowerup = true;
+            powerUpIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerUpIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Collider with: " +  collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
     }
 }
